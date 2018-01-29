2da724ca469742ddefdb9628ed53e803a717ffda
Mees Buschman <mezeman1@gmail.com>
2018-01-29 21:08:40 +0100

    Added word, letter and sentence counter for TextArea

--- src/wordcounter/FXMLDocumentController.java
+++ src/wordcounter/FXMLDocumentController.java
@@ -6,30 +6,97 @@
 package wordcounter;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
 
 /**
  *
  * @author mezem
  */
 public class FXMLDocumentController implements Initializable {
+
     @FXML
-    private Label wordCountLabel;
+    private Label wordCountLabelFill;
     @FXML
-    private Label dotCountLabel;
+    private Label tokenCountLabelFill;
     @FXML
-    private Label sentenceCountLabel;
+    private Label sentenceCountLabelFill;
     @FXML
     private TextArea wordCounter;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
-    }    
-    
+        //On key released eventlistener.
+        wordCounter.addEventHandler(KeyEvent.KEY_RELEASED, new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                checkChanged();
+            }
+        });
+        
+    }
+
+    private void checkChanged() {
+        String text = wordCounter.getText();
+        countLetters(text);
+        countWords(text);
+        countSentence(text);
+    }
+
+    private void countLetters(String text) {
+        String[][] replacements = {{".", ""},
+            {"!", ""}, {"?", ""}, {",", ""}, {" ", ""},
+            {"<", ""}, {">", ""}, {"'", ""}, {"\"", ""},
+            {"@", ""}, {"#", ""}, {"$", ""}, {"%", ""},
+            {"^", ""}, {"&", ""}, {"*", ""}, {"(", ""},
+            {")", ""}, {"-", ""}, {"+", ""}, {"=", ""},
+            {"_", ""}, {"`", ""}, {"/", ""}, {":", ""},
+            {"{", ""}, {"[", ""}, {"]", ""}, {";", ""},
+            {"}", ""}, {"\\", ""}, {"|", ""}};
+        
+        String toCheck = text;
+        for(String[] replacement : replacements){
+            toCheck = toCheck.replace(replacement[0], replacement[1]);
+        }
+        
+        tokenCountLabelFill.setText(" " + toCheck.length());
+    }
+
+    private void countWords(String text) {
+       String toCheck = text;
+       //TODO: Multiple spaces adds words.
+       String[] words = toCheck.split(" ");
+       int amountOfWords = words.length;
+        for(String x : words){
+            if(x.isEmpty()){
+                amountOfWords = amountOfWords - 1;
+                System.out.println(x);
+            }
+        }
+       
+       
+       
+       wordCountLabelFill.setText(" " + amountOfWords);
+    }
+
+    private void countSentence(String text) {
+        String toCheck = text;
+        
+        //TODO: Fix problem placing multiple characters adding sentence.
+        int amountOfSentences = toCheck.split("[!?.:]+").length;;
+
+        sentenceCountLabelFill.setText(" " + amountOfSentences);
+    }
 }
